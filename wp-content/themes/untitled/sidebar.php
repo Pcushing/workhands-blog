f5d8cc7cf728902261f117779bb24d94cb12804a
Paul Zaich <pzaich@gmail.com>
2014-04-04 17:25:27 -0700

    add a get started and description to sidebar

--- wp-content/themes/untitled/sidebar.php
+++ wp-content/themes/untitled/sidebar.php
@@ -4,9 +4,14 @@
  *
  * @package untitled
  */
-
 if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
 <div id="secondary" class="widget-area" role="complementary">
+  <aside class="widget widget_categories" >
+    <h1 class="widget-title">Who we are</h1>
+    <p>WorkHands is the free, nationwide network built exclusively for workers in the skilled trades</p>
+    <a href="http://workhands.us" class="sidebar-button">Get started</a>
+  </aside>
 	<?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary -->
 <?php endif;
+
